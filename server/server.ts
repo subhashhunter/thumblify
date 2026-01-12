bc455e314a267ab3e13cf1d410ac3a77e4981b73
subhashhunter <subhash56190@gmail.com>
2026-01-13 01:38:19 +0530

    update server.ts

--- server/server.ts
+++ server/server.ts
@@ -20,16 +20,24 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-     origin: ['http://localhost:5173','http://localhost:3000'],
+     origin: ['http://localhost:5173','http://localhost:3000','https://thumblify-clinet.vercel.app'],
      credentials:true
 
 }))
+
+app.set('trust proxy',1)
 
 app.use(session({
     secret:process.env.SESSION_SECRET  as string,
     resave:false,
     saveUninitialized:false,
-    cookie:{maxAge:1000*60*60*24*7},
+    cookie:{
+        maxAge:1000*60*60*24*7,
+        httpOnly:true,
+        secure:process.env.NODE_ENV==='production',
+        sameSite:process.env.NODE_ENV==='production' ? 'none':'lax',
+        path:'/'
+    },
     store:MongoStore.create({
         mongoUrl:process.env.MONGODB_URI as string,
         collectionName:'sessions'
